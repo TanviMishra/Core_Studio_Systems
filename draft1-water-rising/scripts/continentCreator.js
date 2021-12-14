d129fa95ff094d16f2b812406c03f3277bbbf566
TanviMishra <misht490@newschool.edu>
2021-12-13 21:30:59 -0500

    json update to arrays

--- draft1-water-rising/scripts/continentCreator.js
+++ draft1-water-rising/scripts/continentCreator.js
@@ -3,12 +3,24 @@ fetch(url).then(response => {
     return response.json();
   }).then(data => {
     // Work with JSON data here
-    maxTop=data.China.altitude.high;
-    minBottom=data.China.altitude.low;
-    countryTop = data.Belgium.altitude.high;
-    countryBottom = data.Belgium.altitude.low;
-    countryTopFormatted=scale(countryTop,maxTop,minBottom,window.innerHeight,0)
-    console.log(countryTop, "and", countryTopFormatted)
+    // maxTop=data.China.altitude.high;
+    // minBottom=data.China.altitude.low;
+    // countryTop = data.China.altitude.high;
+    // countryBottom = data.Belgium.altitude.low;
+    // countryTopFormatted=scale(countryTop,maxTop,minBottom,window.innerHeight,0)
+    // console.log(countryTop, "and", countryTopFormatted)
+
+    // the code you're looking for
+    console.log(data)
+    let search = 'China';
+    for (let i = 0; i < data.length; i++){
+    if (data[i].country == search){
+        console.log(data[i].altitude.low)
+    }
+    else{
+        console.log("not found")
+    }
+    }
     
   }).catch(err => {
     console.log("error loading data")
