3231b88ef1d7c42ad0580b0522fa5bb88bb33c04
TanviMishra <misht490@newschool.edu>
2021-12-13 20:55:54 -0500

    json error fix

--- draft1-water-rising/scripts/continentCreator.js
+++ draft1-water-rising/scripts/continentCreator.js
@@ -1,2 +1,9 @@
-// var countryData = JSON.parse(country.JSON);
-// console.log(countryData)
+// Replace ./data.json with your JSON feed
+fetch("./country.json").then(response => {
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    console.log(data);
+  }).catch(err => {
+    // Do something for an error here
+  });
