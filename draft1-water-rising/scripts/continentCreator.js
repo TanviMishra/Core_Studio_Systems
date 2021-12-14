0058593894d96c1d7e7d92845e7bbcbd5efa1c34
TanviMishra <misht490@newschool.edu>
2021-12-13 21:17:49 -0500

    change json

--- draft1-water-rising/scripts/continentCreator.js
+++ draft1-water-rising/scripts/continentCreator.js
@@ -3,12 +3,12 @@ fetch(url).then(response => {
     return response.json();
   }).then(data => {
     // Work with JSON data here
-    maxTop=data.china.altitude.high;
-    minBottom=data.china.altitude.low;
+    maxTop=data.China.altitude.high;
+    minBottom=data.China.altitude.low;
     countryTop = data.Belgium.altitude.high;
     countryBottom = data.Belgium.altitude.low;
-    countryTopFormatted=scale(countryTop,maxTop,minBottom,0,window.innerHeight)
-    
+    countryTopFormatted=scale(countryTop,maxTop,minBottom,window.innerHeight,0)
+    console.log(countryTop, "and", countryTopFormatted)
     
   }).catch(err => {
     console.log("error loading data")
